d994478effc5cbc2e19852c5fbf41dca12cf4460
Hasanc20 <258aslan@gmail.com>
2024-06-11 18:41:39 +0200

    Figur kann sich nach links und rechts bewegen

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -12,18 +12,29 @@ public class Player : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    public float moveSpeed = 5f; // Geschwindigkeit der Bewegung
+
     // Update is called once per frame
     void Update()
     {
-        // Überprüfen, ob die linke Pfeiltaste gedrückt wurde
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // Bewegung in horizontaler Richtung basierend auf der Pfeiltaste
+        float moveHorizontal = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             animator.SetTrigger("WalkLeft");
+            moveHorizontal = -1f; // Bewegung nach links
         }
-        // Überprüfen, ob die rechte Pfeiltaste gedrückt wurde
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             animator.SetTrigger("WalkRight");
+            moveHorizontal = 1f; // Bewegung nach rechts
         }
+
+        // Berechnung der Bewegung basierend auf der Tasteneingabe
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f) * moveSpeed * Time.deltaTime;
+
+        // Bewegung anwenden
+        transform.Translate(movement);
     }
 }
