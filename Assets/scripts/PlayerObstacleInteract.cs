7a4f141583b034de718ed7906a71cc014c526c9e
Özge Özenoglu <o.oezenoglu@outlook.de>
2024-06-29 15:48:16 +0200

    add puddle obstacle + player interaction

--- Assets/scripts/PlayerObstacleInteract.cs
+++ Assets/scripts/PlayerObstacleInteract.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTouch : MonoBehaviour
+public class PlayerObstacleInteract : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Spike"))
+        if (collision.gameObject.CompareTag("Spike") || collision.gameObject.CompareTag("Puddle"))
         {
             Player.PlayerIsAlive = false;
             Destroy(gameObject);
